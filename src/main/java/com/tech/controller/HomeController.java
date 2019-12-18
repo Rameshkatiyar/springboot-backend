0be1aba000a652aa0a73711040ed112b363cf88a
ramesh75 <ramesh.katiyar@evivehealth.com>
2019-12-18 21:55:13 +0530

    Added user model.

--- src/main/java/com/tech/controller/HomeController.java
+++ src/main/java/com/tech/controller/HomeController.java
@@ -1,14 +1,26 @@
 package com.tech.controller;
 
+import com.tech.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@Slf4j
+@CrossOrigin //For Cross Origin, follow it: https://stackoverflow.com/questions/35091524/spring-cors-no-access-control-allow-origin-header-is-present
+@RestController
 public class HomeController {
-    @GetMapping("/user")
-    public ResponseEntity<?> getUserDetails(@RequestParam(value = "userId", required = false, defaultValue = "foo") String userId){
-        return ResponseEntity.ok("Welcome User: "+userId);
+
+    @GetMapping(value = "/user")
+    public ResponseEntity<?> getUserDetails(@RequestParam(value = "userId", required = false, defaultValue = "12345") String userId){
+        log.info("Sending Response For User: {}", userId);
+        User user = User.builder()
+                .userId(userId)
+                .firstName("Seth")
+                .lastName("Tim")
+                .build();
+        return ResponseEntity.ok(user);
     }
 }
---
+++ src/main/java/com/tech/model/User.java
@@ -0,0 +1,16 @@
+package com.tech.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    String userId;
+    String firstName;
+    String lastName;
+}
